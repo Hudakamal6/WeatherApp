9953b45e3e20a1c9a4fafbf709bcd6a551d1549f
huda <Hudakamal6>
2023-03-12 05:58:18 +0200

    home ui

--- app/src/main/java/com/example/weatherapp/homescreen/view/HomeFragment.kt
+++ app/src/main/java/com/example/weatherapp/homescreen/view/HomeFragment.kt
@@ -2,39 +2,54 @@ package com.example.weatherapp.homescreen.view
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.airbnb.lottie.LottieAnimationView
 import com.bumptech.glide.Glide
-import com.example.safyweather.db.LocalSource
 import com.example.weatherapp.R
+import com.example.weatherapp.database.LocalSource
 import com.example.weatherapp.databinding.FragmentHomeBinding
 import com.example.weatherapp.homescreen.viewmodel.HomeViewModel
 import com.example.weatherapp.homescreen.viewmodel.HomeViewModelFactory
 import com.example.weatherapp.model.Repository
+import com.example.weatherapp.model.Settings
 import com.example.weatherapp.model.WeatherForecast
 import com.example.weatherapp.network.RemoteSource
+import com.example.weatherapp.utilities.Constants.MY_SHARED_PREFERENCES
 import com.example.weatherapp.utilities.Convertors
-import com.example.weatherapp.utilities.MY_SHARED_PREFERENCES
+import com.example.weatherapp.utilities.CurrentUser
+import com.example.weatherapp.utilities.LocalHelper
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 
 class HomeFragment : Fragment() {
     lateinit var viewModelFactory: HomeViewModelFactory
-    lateinit var animLoading: LottieAnimationView
-    lateinit var viewModel: HomeViewModel
+    lateinit var homeViewModel: HomeViewModel
     lateinit var hourlyAdapter: HourlyWeatherAdapter
     lateinit var dailyAdapter: DailyWeatherAdapter
     lateinit var layoutManagerHourly: LinearLayoutManager
     lateinit var layoutManagerDaily: LinearLayoutManager
     lateinit var binding: FragmentHomeBinding
+    lateinit var settings : Settings
+    val args: HomeFragmentArgs by navArgs()
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModelFactory = HomeViewModelFactory( Repository(RemoteSource.getInstance(),
+            LocalSource.getInstance(requireContext()),requireContext(),requireContext().getSharedPreferences(MY_SHARED_PREFERENCES, Context.MODE_PRIVATE))
+        )
+        homeViewModel = ViewModelProvider(this,viewModelFactory).get(HomeViewModel::class.java)
+        settings = Settings()
+        getData()
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -42,33 +57,29 @@ class HomeFragment : Fragment() {
 
         return inflater.inflate(R.layout.fragment_home, container, false)
 
-
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentHomeBinding.bind(view)
-        viewModelFactory = HomeViewModelFactory(Repository.getInstance(RemoteSource.getInstance()
-            ,LocalSource.getInstance(requireActivity())
-            ,requireContext()
-            ,requireContext().getSharedPreferences(MY_SHARED_PREFERENCES, Context.MODE_PRIVATE)))
 
-        viewModel = ViewModelProvider(this, viewModelFactory).get(HomeViewModel::class.java)
-        animLoading = view.findViewById(R.id.animationView)
 
-         lifecycleScope.launch{   setupRecyclerViews()
-            applyUIChange(viewModel._repo.getCurrentWeatherWithLocationInRepo(30.0444,31.2357,"metric"))
-             hourlyAdapter.notifyDataSetChanged()
-             dailyAdapter.notifyDataSetChanged()}
 
     }
+    fun getData() {
+        lifecycleScope.launch (Dispatchers.Main){
+            val weather = homeViewModel.getWeather(args.lat.toDouble(),args.longg.toDouble())
+            updateUi(weather)
+            setupRecyclerViews()
+
+        }
+    }
+
 
     fun setupRecyclerViews() {
-        hourlyAdapter = HourlyWeatherAdapter(context as Context, arrayListOf(), "metric")
-        dailyAdapter = DailyWeatherAdapter(context as Context, arrayListOf(), "metric")
-        layoutManagerHourly =
-            LinearLayoutManager(context as Context, LinearLayoutManager.HORIZONTAL, false)
+        layoutManagerHourly =LinearLayoutManager(context as Context, LinearLayoutManager.HORIZONTAL, false)
         layoutManagerDaily = LinearLayoutManager(context as Context)
         binding.hourlyRecycler.adapter = hourlyAdapter
         binding.dailyRecycler.adapter = dailyAdapter
@@ -76,26 +87,33 @@ class HomeFragment : Fragment() {
         binding.dailyRecycler.layoutManager = layoutManagerDaily
     }
 
-    fun applyUIChange(currWeather: WeatherForecast?) {
-        currWeather as WeatherForecast
-        animLoading.visibility = View.GONE
-        binding.currCity.text = currWeather.timezone
-        binding.currDate.text = Convertors.getDateFormat(currWeather.current.dt)
-        binding.currTime.text = Convertors.getTimeFormat(currWeather.current.dt)
-        binding.currTemp.text = currWeather.current.temp.toString()
-        binding.currDesc.text = currWeather.current.weather[0].description
-        Log.i("TAG", "applyUIChange:---------------------------------------- ${currWeather.current.weather[0].description}")
-        binding.currHumidity.text = currWeather.current.humidity.toString()
-        binding.currWindSpeed.text = currWeather.current.wind_speed.toString()
-        binding.currClouds.text = currWeather.current.clouds.toString()
-        binding.currPressure.text = currWeather.current.pressure.toString()
+    fun updateUi(currentWeather: WeatherForecast?) {
+        currentWeather as WeatherForecast
 
-        binding.currUnit.text = getString(R.string.Celsius)
-        binding.currWindUnit.text = getString(R.string.windMeter)
+        binding.currCity.text = LocalHelper.getAddressFromLatLng(requireActivity(),
+            CurrentUser.location.latitude
+            ,CurrentUser.location.longitude
+        )
+
+       binding.currDate.text = Convertors.getDateFormat(currentWeather.current.dt)
+        binding.currTime.text = Convertors.getTimeFormat(currentWeather.current.dt)
+        binding.tvTempreture.text = currentWeather.current.temp.toString()
+        binding.tvDiscription.text = currentWeather.current.weather[0].description
+        binding.tvHumidity.text = currentWeather.current.humidity.toString()
+        binding.tvWindSpeed.text = currentWeather.current.wind_speed.toString()
+        binding.tvClouds.text = currentWeather.current.clouds.toString()
+        binding.tvPressure.text = currentWeather.current.pressure.toString()
+        binding.tvWindSpeedUnit.text = getString(R.string.windMile)
+        //use picasso
         Glide.with(context as Context)
-            .load("https://openweathermap.org/img/wn/"+currWeather.current.weather[0].icon+"@2x.png")
-            .into(binding.currIcon)
-        hourlyAdapter.setHourlyWeatherList(currWeather.hourly)
-        dailyAdapter.setDailyWeatherList(currWeather.daily)
+            .load("https://openweathermap.org/img/wn/"+currentWeather.current.weather[0].icon+"@2x.png")
+            .into(binding.ivIcon)
+
+        hourlyAdapter = HourlyWeatherAdapter(requireContext(),currentWeather.hourly,settings.unit.toString())
+
+        dailyAdapter = DailyWeatherAdapter(requireContext(),currentWeather.daily,settings.unit.toString())
+
+
+
     }
 }
--- app/src/main/java/com/example/weatherapp/homescreen/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/weatherapp/homescreen/viewmodel/HomeViewModel.kt
@@ -1,28 +1,53 @@
 package com.example.weatherapp.homescreen.viewmodel
 
+import android.content.ContentValues.TAG
 import android.util.Log
 import androidx.lifecycle.*
 import com.example.weatherapp.model.Repository
-import com.example.weatherapp.model.RepositoryInterface
+import com.example.weatherapp.model.Settings
 import com.example.weatherapp.model.WeatherForecast
-import com.example.weatherapp.network.RetroFitState
+import com.google.android.gms.maps.model.LatLng
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 
-class HomeViewModel(val _repo:RepositoryInterface): ViewModel() {
+class HomeViewModel(val _repo:Repository): ViewModel() {
 
-    private val _weatherFromNetwork = MutableLiveData<WeatherForecast>()
-    val weatherFromNetwork: LiveData<WeatherForecast> = _weatherFromNetwork
 
-    fun getWholeWeather(lat: Double, long: Double, unit: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            Log.i("TAG", "getWholeWeather: in view model ")
-            val response = _repo.getCurrentWeatherWithLocationInRepo(lat,long,unit)
-            _weatherFromNetwork.postValue(response)
+    suspend fun getWeather(lat: Double, long: Double): WeatherForecast {
+        var weather: WeatherForecast? = null
+        val job = viewModelScope.launch(Dispatchers.IO) {
+            weather = _repo.getCurrentWeatherWithLocationInRepo(lat, long, "metric")
         }
+        job.join()
+
+        Log.i(TAG, "getWeather: ${weather!!.hourly[0].weather}")
+
+        return weather as WeatherForecast
+
     }
+
+
+    fun getLocationSP(): LatLng? {
+        var location = _repo.getWeatherWithShP()
+        return location
+    }
+
+    fun getStoredSettings(): Settings? {
+        return _repo.getSettingsSharedPreferences()
+    }
+}
+
+
+//    private val _weatherFromNetwork = MutableLiveData<WeatherForecast>()
+//    val weatherFromNetwork: LiveData<WeatherForecast> = _weatherFromNetwork
+//
+//    fun getWholeWeather(lat: Double, long: Double, unit: String) {
+//        viewModelScope.launch(Dispatchers.IO) {
+//            Log.i("TAG", "getWholeWeather: in view model ")
+//            val response = _repo.getCurrentWeatherWithLocationInRepo(lat,long,unit)
+//            _weatherFromNetwork.postValue(response)
+//        }
+//    }
 
 //    var apiState = MutableStateFlow<RetroFitState>(RetroFitState.Loading)
 //
@@ -35,4 +60,3 @@ class HomeViewModel(val _repo:RepositoryInterface): ViewModel() {
 //            apiState.value = RetroFitState.onSuccess(currentWeather)
 //        }
 //    }
-}
