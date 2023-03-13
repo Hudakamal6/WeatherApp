33b6b4d6885f486db3d422eeade54f43bf1ac570
huda <Hudakamal6>
2023-03-13 04:19:11 +0200

    navigation

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,28 +1,101 @@
 package com.example.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.drawerlayout.widget.DrawerLayout
+import android.view.View
+import androidx.annotation.NonNull
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.navigation.NavigationView
+import com.example.weatherapp.databinding.ActivityMainBinding
+import com.ismaeldivita.chipnavigation.ChipNavigationBar
 
 class MainActivity : AppCompatActivity() {
-    lateinit var navigationDrawer: DrawerLayout
-    lateinit var navigationView: NavigationView
-    lateinit var navController: NavController
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        navigationDrawer = findViewById(R.id.mainDrawer)
-        navigationView = findViewById(R.id.mainNavView)
+//    lateinit var navigationDrawer: DrawerLayout
+//    lateinit var navigationView: NavigationView
+//    lateinit var navController: NavController
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        navigationDrawer = findViewById(R.id.mainDrawer)
+//        navigationView = findViewById(R.id.mainNavView)
+//
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//        navController = navHostFragment.findNavController()
+//
+//        navigationView.setupWithNavController(navController)
+//
+//    }
+//}
+val bottomChip by lazy { findViewById<ChipNavigationBar>(R.id.bottomChip) }
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.findNavController()
+ private lateinit var navController: NavController
+ private lateinit var binding: ActivityMainBinding
 
-        navigationView.setupWithNavController(navController)
+ override fun onCreate(savedInstanceState: Bundle?) {
+  super.onCreate(savedInstanceState)
+  binding = ActivityMainBinding.inflate(layoutInflater)
+  setContentView(binding.root)
 
+  val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+  navController = navHostFragment.findNavController()
+  bottomChip.setItemSelected(R.id.home)
+  bottomChip.setOnItemSelectedListener { id->
+
+   when(id) {R.id.home->
+    findNavController(this@MainActivity, R.id.nav_host_fragment)
+      .navigate(R.id.homeFragment2)
+
+  R.id.favorites ->
+   findNavController(this@MainActivity,R.id.nav_host_fragment)
+      .navigate(R.id.favoriteFragment)
+
+
+    R.id.alerts->
+     findNavController(this@MainActivity,R.id.nav_host_fragment)
+      .navigate(R.id.alertsFragment)
+
+
+    else ->
+  findNavController(this@MainActivity,R.id.nav_host_fragment)
+      .navigate(R.id.settingsFragment2)
+   }
+  }
+
+  handelNavigationVisual()
+
+ }
+
+
+ fun handelNavigationVisual() {
+  navController.addOnDestinationChangedListener(object :
+   NavController.OnDestinationChangedListener {
+   override fun onDestinationChanged(
+    @NonNull controller: NavController,
+    @NonNull destination: NavDestination,
+    arguments: Bundle?
+   ) {
+    when (destination.id) {
+
+    R.id.splashFragment,
+    R.id.intialFragment,
+   // R.id.mapsFragment
+     -> bottomChip.setVisibility(
+      View.GONE
+     )
+     else -> bottomChip.setVisibility(View.VISIBLE)
     }
+   }
+  })
+ }
+
+
+
+
+
+
+
+
 }
