67f62ed1444ec14574a79724e56f80379c0ce73c
huda <Hudakamal6>
2023-03-15 23:49:01 +0200

    alerts

--- app/src/main/java/com/example/weatherapp/alerts/view/AlertsAdapter.kt
+++ app/src/main/java/com/example/weatherapp/alerts/view/AlertsAdapter.kt
@@ -1,67 +1,121 @@
 package com.example.weatherapp.alerts.view
 
 import android.content.Context
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherapp.R
+import com.example.weatherapp.databinding.AlertItemLayoutBinding
 import com.example.weatherapp.model.AlertData
 import com.example.weatherapp.utilities.Convertors
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import java.util.*
 
-class AlertsAdapter : RecyclerView.Adapter<AlertsAdapter.AlertsViewHolder> {
+class AlertsAdapter (private val lifeCycleScopeInput: CoroutineScope, var onClickHandler: OnClickAlertListener
+)
+    : ListAdapter<AlertData, AlertsAdapter.AlertViewHolder>(AlertDiffUtil()) {
+    class AlertViewHolder(var binding: AlertItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {}
 
-    private var context: Context
-    private var alerts:List<AlertData>
-    private var onClickHandler: OnClickAlertListener
+    class AlertDiffUtil : DiffUtil.ItemCallback<AlertData>() {
+        override fun areItemsTheSame(oldItem: AlertData, newItem: AlertData): Boolean {
+            return (oldItem.idHashLongFromLonLatStartStringEndStringAlertType == newItem.idHashLongFromLonLatStartStringEndStringAlertType)
+        }
 
-    constructor(context: Context, alerts:List<AlertData>, onClickHandler: OnClickAlertListener){
-        this.context = context
-        this.alerts = alerts
-        this.onClickHandler = onClickHandler
+        override fun areContentsTheSame(oldItem: AlertData, newItem: AlertData): Boolean {
+            return oldItem == newItem
+        }
+
     }
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): AlertsViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.alert_item_layout,parent,false)
-        return AlertsViewHolder(view)
+    lateinit var binding: AlertItemLayoutBinding
+
+    lateinit var sharedPreferences: SharedPreferences
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlertViewHolder {
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(parent.context)
+
+        val inflater: LayoutInflater =
+            parent.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        binding = AlertItemLayoutBinding.inflate(inflater, parent, false)
+        return AlertViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: AlertsViewHolder, position: Int) {
-        holder.fromTime.text = "${alerts[position].fromDate.hours}:${alerts[position].fromDate.minutes}"
-        holder.fromDate.text = Convertors.getDateFromInt(alerts[position].fromDate.date,alerts[position].fromDate.month,alerts[position].fromDate.year)
-        holder.toTime.text = "${alerts[position].toDate.hours}:${alerts[position].toDate.minutes}"
-        holder.toDate.text = Convertors.getDateFromInt(alerts[position].toDate.date,alerts[position].toDate.month,alerts[position].toDate.year)
-        holder.removeBtn.setOnClickListener{onClickHandler.onRemoveAlertClickListener(alerts[position])}
-    }
+    override fun onBindViewHolder(holder: AlertViewHolder, position: Int) {
+        val current = getItem(position)
 
-    override fun getItemCount(): Int {
-        return alerts.size
-    }
+        holder.binding.fromTimeAlert.text = current.address
+//        holder.binding.fromTimeAlert.text = current.startString
+//        holder.binding.fromDateAlert.text = (current.startDT as Date).toString()
 
-    fun setAlertsList(alerts:List<AlertData>){
-        this.alerts = alerts
-    }
+        holder.binding.removeAlert.setOnClickListener {
+            onClickHandler.onRemoveAlertClickListener(current)
+        }
 
-    inner class AlertsViewHolder(private val view: View): RecyclerView.ViewHolder(view){
-        val alertConstraint: ConstraintLayout
-            get() = view.findViewById(R.id.alertItemConstrain)
-        val removeBtn: ImageView
-            get() = view.findViewById(R.id.removeAlert)
-        val fromTime: TextView
-            get() = view.findViewById(R.id.fromTimeAlert)
-        val toTime: TextView
-            get() = view.findViewById(R.id.toTimeAlert)
-        val fromDate: TextView
-            get() = view.findViewById(R.id.fromDateAlert)
-        val toDate: TextView
-            get() = view.findViewById(R.id.toDateAlert)
-    }
+        holder.binding.fromDateAlert.text = current.startString
 
+
+    }
 }
+
+
+
+
+//    private var context: Context
+//    private var alerts:List<AlertData>
+//    private var onClickHandler: OnClickAlertListener
+//
+//    constructor(context: Context, alerts:List<AlertData>, onClickHandler: OnClickAlertListener){
+//        this.context = context
+//        this.alerts = alerts
+//        this.onClickHandler = onClickHandler
+//    }
+//
+//    override fun onCreateViewHolder(
+//        parent: ViewGroup,
+//        viewType: Int
+//    ): AlertsViewHolder {
+//        val view = LayoutInflater.from(parent.context).inflate(R.layout.alert_item_layout,parent,false)
+//        return AlertsViewHolder(view)
+//    }
+//
+//    override fun onBindViewHolder(holder: AlertsViewHolder, position: Int) {
+//        holder.fromTime.text = "${alerts[position].fromDate.hours}:${alerts[position].fromDate.minutes}"
+//        holder.fromDate.text = Convertors.getDateFromInt(alerts[position].fromDate.date,alerts[position].fromDate.month,alerts[position].fromDate.year)
+//        holder.toTime.text = "${alerts[position].toDate.hours}:${alerts[position].toDate.minutes}"
+//        holder.toDate.text = Convertors.getDateFromInt(alerts[position].toDate.date,alerts[position].toDate.month,alerts[position].toDate.year)
+//        holder.removeBtn.setOnClickListener{onClickHandler.onRemoveAlertClickListener(alerts[position])}
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return alerts.size
+//    }
+//
+//    fun setAlertsList(alerts:List<AlertData>){
+//        this.alerts = alerts
+//    }
+//
+//    inner class AlertsViewHolder(private val view: View): RecyclerView.ViewHolder(view){
+//        val alertConstraint: ConstraintLayout
+//            get() = view.findViewById(R.id.alertItemConstrain)
+//        val removeBtn: ImageView
+//            get() = view.findViewById(R.id.removeAlert)
+//        val fromTime: TextView
+//            get() = view.findViewById(R.id.fromTimeAlert)
+//        val toTime: TextView
+//            get() = view.findViewById(R.id.toTimeAlert)
+//        val fromDate: TextView
+//            get() = view.findViewById(R.id.fromDateAlert)
+//        val toDate: TextView
+//            get() = view.findViewById(R.id.toDateAlert)
+//    }
+
--- app/src/main/java/com/example/weatherapp/alerts/viewmodel/AlertsViewModel.kt
+++ app/src/main/java/com/example/weatherapp/alerts/viewmodel/AlertsViewModel.kt
@@ -1,53 +1,70 @@
 package com.example.weatherapp.alerts.viewmodel
 
-import android.content.ContentValues
-import android.content.ContentValues.TAG
-import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.weatherapp.database.AlertState
 import com.example.weatherapp.model.AlertData
-import com.example.weatherapp.model.Repository
-import com.example.weatherapp.model.Settings
+import com.example.weatherapp.model.RepositoryInterface
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class AlertsViewModel(private val repo:Repository) :ViewModel(){
+class AlertsViewModel(private val repo:RepositoryInterface) :ViewModel(){
 
 
-    var alertState = MutableStateFlow<AlertState>(AlertState.Loading)
 
-    fun addAlert(alert: AlertData) {
-        repo.insertAlert(alert)
-        Log.i(ContentValues.TAG, "insert alerttt VIEWWWW MODELLLLLLLLLLLLLLLLLLLLLLLLLLLL ${alert}")
-    }
-    fun deleteAlert(alert: AlertData) {
-        repo.deleteAlert(alert)
+    fun getAllAlertsInVM(): LiveData<List<AlertData>> {
+        return repo.getAllAlertsInRepo()
     }
 
-    fun getAllAlert(): MutableStateFlow<AlertState> {
-        var alertList = mutableListOf<AlertData>()
-        viewModelScope.launch(Dispatchers.IO) {
-            repo.storedAlerts().catch {
-                alertState.value = AlertState.onFail(it)
-                Log.i(TAG, "errrrroooooooooooooooooooooooooooooooor in viewwwwww model ${it.toString()}")
-            }.collect{
-              //  alertList.add(it)
-                alertList = it as MutableList<AlertData>
-                alertState.value = AlertState.onSuccessList(it)
-                Log.i(ContentValues.TAG, "GET ALERT list IN VIEWW MODELLLLLLLLLLLLLLLLLLLL ${it} ")
-            }
-
-            Log.i(ContentValues.TAG, "GET ALERT IN VIEWW MODELLLLLLLLLLLLLLLLLLLL ${alertList}")
+    fun addAlertInVM(alert:AlertData){
+        viewModelScope.launch(Dispatchers.IO){
+            repo.insertAlert(alert)
         }
-        return alertState
-        Log.i(ContentValues.TAG, "GET ALL IN VIEW MODELLLLLLLLLLLLLLLLLLLLLL RETURN ALERT STATE  ")
     }
-    fun getStoredSettings(): Settings? {
-        return repo.getSettingsSharedPreferences()
+
+    fun removeAlertInVM(alert:AlertData){
+        viewModelScope.launch(Dispatchers.IO){
+            repo.deleteAlert(alert)
+        }
     }
+
+//    fun getStoredSettings(): Settings? {
+//        return repo.getSettingsSharedPreferences()
+//    }
+
+
+
+   // var alertState = MutableStateFlow<AlertState>(AlertState.Loading)
+//
+//    fun addAlert(alert: AlertData) {
+//        repo.insertAlert(alert)
+//        Log.i(ContentValues.TAG, "insert alerttt VIEWWWW MODELLLLLLLLLLLLLLLLLLLLLLLLLLLL ${alert}")
+//    }
+//    fun deleteAlert(alert: AlertData) {
+//        repo.deleteAlert(alert)
+//    }
+
+//    fun getAllAlert(): MutableStateFlow<AlertState> {
+//        var alertList = mutableListOf<AlertData>()
+//        viewModelScope.launch(Dispatchers.IO) {
+//            repo.storedAlerts().catch {
+//                alertState.value = AlertState.onFail(it)
+//                Log.i(TAG, "errrrroooooooooooooooooooooooooooooooor in viewwwwww model ${it.toString()}")
+//            }.collect{
+//
+//                alertList = it as MutableList<AlertData>
+//                alertState.value = AlertState.onSuccessList(it)
+//                Log.i(ContentValues.TAG, "GET ALERT list IN VIEWW MODELLLLLLLLLLLLLLLLLLLL ${it} ")
+//            }
+//
+//            Log.i(ContentValues.TAG, "GET ALERT IN VIEWW MODELLLLLLLLLLLLLLLLLLLL ${alertList}")
+//        }
+//        return alertState
+//        Log.i(ContentValues.TAG, "GET ALL IN VIEW MODELLLLLLLLLLLLLLLLLLLLLL RETURN ALERT STATE  ")
+//    }
 
 
 }
